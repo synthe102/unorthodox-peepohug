17d4964a2669fa520865ecf4ceff3df375e93d19
synthe102 <leonard.suslian@gmail.com>
2021-10-12 14:36:34 +0200

    feat: Add comments on tf code.

--- main.tf
+++ main.tf
@@ -1,11 +1,14 @@
+# Return the arn of the container repository for our image.
 data "aws_ecr_repository" "webapp" {
   name = var.container
 }
 
+# Default role for AppRunner to access a private ECR registry.
 data "aws_iam_role" "AppRunnerECRAccessRole" {
   name = "AppRunnerECRAccessRole"
 }
 
+# Create an AppRunner service that will run our container and manage load balancing, ssl certificates and scalling.
 resource "aws_apprunner_service" "webapp" {
   service_name = var.service_name
   source_configuration {
@@ -28,37 +31,42 @@ resource "aws_apprunner_service" "webapp" {
   }
 }
 
+# Autoscaling configuration, 2 instances min ( to demonstrate load balancing )
 resource "aws_apprunner_auto_scaling_configuration_version" "webapp_autoscalling" {
   auto_scaling_configuration_name = var.service_name
   max_concurrency                 = 50
-  min_size                        = 3
-  max_size                        = 5
+  min_size                        = 2
+  max_size                        = 3
 
   tags = {
     Name = "${var.service_name}-autoscaling-config"
   }
 }
 
+# Custom domain used for accessing the webapp
 resource "aws_apprunner_custom_domain_association" "suslian_engineer" {
   domain_name          = "${var.custom_subdomain}.${var.custom_domain}"
   service_arn          = aws_apprunner_service.webapp.arn
   enable_www_subdomain = false
 }
 
+# Cloudflare zone used with the custom domain
 data "cloudflare_zones" "suslian_engineer" {
   filter {
     name = var.custom_domain
   }
 }
 
+# Automatic http to https redirection
 resource "cloudflare_page_rule" "https" {
   zone_id = data.cloudflare_zones.suslian_engineer.zones[0].id
   target  = "*.${var.custom_domain}/*"
   actions {
-    always_use_https = true
+    automatic_https_rewrites = "on"
   }
 }
 
+# Create the custom domain record on Cloudflare
 resource "cloudflare_record" "suslian_engineer" {
   zone_id = data.cloudflare_zones.suslian_engineer.zones[0].id
   name    = var.custom_subdomain
@@ -66,6 +74,7 @@ resource "cloudflare_record" "suslian_engineer" {
   type    = "CNAME"
 }
 
+# Create the DNS records required for the SSL certificate validation
 resource "cloudflare_record" "aws_acm_validation" {
   count   = length(aws_apprunner_custom_domain_association.suslian_engineer.certificate_validation_records)
   zone_id = data.cloudflare_zones.suslian_engineer.zones[0].id
--- variables.tf
+++ variables.tf
@@ -1,19 +1,23 @@
 variable "custom_domain" {
   type    = string
   default = "suslian.engineer"
+  description = "The custom domain to associate with the AppRunner service"
 }
 
 variable "custom_subdomain" {
   type    = string
   default = "webapp"
+  description = "The custom subdomain to associate with the AppRunner service"
 }
 
 variable "container" {
   type    = string
   default = "webapp"
+  description = "Name of the image repository on ECR"
 }
 
 variable "service_name" {
   type    = string
   default = "webapp"
+  description = "Name to give to the AppRunner service"
 }
